3126e23e486128cd40fed1c90f1791b68dffdbf2
Clarefollett <chlaw101@gmail.com>
2017-04-05 08:40:58 -0700

    console loggies

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const http = require('http');
 
 function onRequest(request, response) {
+    console.log('request received')
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write('Hello World');
     response.end();
 }
 
 http.createServer(onRequest).listen(8888);
+console.log('server has started')
