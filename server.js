d538c72e79c78132fdd4fb641a565a590f3ba758
Clarefollett <chlaw101@gmail.com>
2017-04-05 08:43:09 -0700

    refactored server

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const http = require('http');
 
-function onRequest(request, response) {
-    console.log('request received')
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello World');
-    response.end();
+function start() {
+    function onRequest(request, response) {
+        console.log('request received')
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write('Hello World');
+        response.end();
+    }
+    http.createServer(onRequest).listen(8888);
+    console.log('server has started')
 }
 
-http.createServer(onRequest).listen(8888);
-console.log('server has started')
+exports.start = start;
